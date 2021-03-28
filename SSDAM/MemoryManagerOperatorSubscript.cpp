b08fc1cab0a946bbdd0c5795cdc46ab3a5536c88
Nikhil Nayyar <nikhilnayyar002@gmail.com>
2021-03-28 13:11:01 +0530

    MemoryManagerOperatorSubscript

---
+++ SSDAM/MemoryManagerOperatorSubscript.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+#include <chrono>
+using namespace std;
+
+#define BASE_SIZE 1000
+#define NO_OF_TIMES 500000
+
+template <class T, size_t countT>
+class MemoryManager
+{
+private:
+    T *startRef;
+    int index;
+
+    const size_t typeSize;
+    const size_t count;
+
+public:
+    MemoryManager() : count(countT), typeSize(sizeof(T)), index(0)
+    {
+        if (!countT)
+        {
+            throw "no of objs that can be allocated cannot be zero.";
+        }
+        try
+        {
+            startRef = reinterpret_cast<T *>(malloc(count * typeSize));
+        }
+        catch (...)
+        {
+            cout << "Array Memory cannot be initialized.";
+            throw; //Re-throwing
+        }
+    }
+
+    void cleanUp()
+    {
+        if (startRef)
+            free(startRef);
+    }
+
+    T *nxtAddress()
+    {
+        if (index == count)
+            index = 0;
+
+        return startRef + index++;
+    }
+
+    void freeAddress(T *objPtr)
+    {
+        if (objPtr)
+        {
+            objPtr->~T();
+            // objPtr = nullptr;
+        }
+    }
+
+    T &operator[](int i)
+    {
+        return startRef[i];
+    }
+
+    ~MemoryManager()
+    {
+        cleanUp();
+    }
+};
+
+class MyPracticalClass
+{
+public:
+    int a, b;
+    void inialize(int a, int b)
+    {
+        MyPracticalClass::a = a;
+        MyPracticalClass::b = b;
+    }
+};
+
+int main()
+{
+    // unsync the I/O of C and C++.
+    ios_base::sync_with_stdio(false);
+
+    auto start = chrono::high_resolution_clock::now();
+
+    MemoryManager<MyPracticalClass, BASE_SIZE> memMan;
+
+    MyPracticalClass *arra[BASE_SIZE];
+
+    //performing 1000 allocation and deletion * 500000 times
+    // 1 Billion operations
+    for (int i = 0; i < NO_OF_TIMES; i++)
+    {
+        for (int j = 0; j < BASE_SIZE; j++)
+            memMan[j].inialize(i, j);
+        for (int j = 0; j < BASE_SIZE; j++)
+            memMan[j].~MyPracticalClass();
+    }
+    // Recording end time.
+    auto end = chrono::high_resolution_clock::now();
+
+    // Calculating total time taken by the program.
+    cout << "Time taken by Overloaded allocaters/deallocaters is : " << chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << endl;
+    return 0;
+}
